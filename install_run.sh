1f9414ad6c66e03acbfa518cb25e29c3e21ed5d3
dhruvagja8808 <dhruvagja8808@gmail.com>
2020-06-08 12:12:00 +0530

    updated install_run.sh

--- install_run.sh
+++ install_run.sh
@@ -2,4 +2,16 @@ cd src
 make
 sudo make install
 cd ..
-./test.sh
+
+read -p "Which test you want to run 1.Test1 2.Test2 " choice
+
+if [ $choice = "1" ]
+then 
+    ./test1.sh
+    echo "Test1 is loaded successfully!"
+fi
+if [ $choice = "2" ]
+then 
+    ./test2.sh
+    echo "Test2 is loaded successfully!"
+fi
